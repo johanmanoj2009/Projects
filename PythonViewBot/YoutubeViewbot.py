e89cf3683cf9923f67a8cb54b297905516b5c6d7
johanmanoj2009 <92201284+johanmanoj2009@users.noreply.github.com>
2021-12-25 13:02:15 +0530

    Update YoutubeViewbot.py

--- PythonViewBot/YoutubeViewbot.py
+++ PythonViewBot/YoutubeViewbot.py
@@ -11,13 +11,13 @@ from selenium import webdriver;
 def window():
     def runBot():
         #time to refresh page (seconds)
-        Timer = int(timeInput.text())
+        Timer = 30 #(30 seconds) 
 
         #youtube link
-        link = linkInput.text()
+        link = 'https://www.youtube.com/watch?v=kma3op9e_3o'
 
         #number of views
-        views = 1000
+        views = 200000
         
 
         driver = webdriver.Chrome('webdrivers\chromedriver.exe')
